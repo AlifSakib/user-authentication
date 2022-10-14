8a59bb169c79ae93ec7518cd1b07eea533212b04
AlifSakib <alifsakib@gmail.com>
2022-10-14 21:11:46 +0600

    added form for signInWIthEmailAndPassword

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Root from "./layout/Root";
 
 function App() {
-  return <div className="App font-poppins"></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      children: [
+        {
+          path: "/",
+          element: <Register></Register>,
+        },
+        {
+          path: "/register",
+          element: <Register></Register>,
+        },
+        {
+          path: "login",
+          element: <Login></Login>,
+        },
+      ],
+    },
+  ]);
+  return (
+    <div className="App font-poppins w-9/12 mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
