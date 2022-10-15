e45c2cf0ffc3fd12b354a00f97d60c2f328b51eb
AlifSakib <alifsakib@gmail.com>
2022-10-15 06:44:01 +0600

    Added user name and email

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,11 @@
 import {
   getAuth,
+  GoogleAuthProvider,
   sendPasswordResetEmail,
   signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  TwitterAuthProvider,
 } from "firebase/auth";
 import { Button, Label, TextInput } from "flowbite-react";
 import React, { useState } from "react";
@@ -12,6 +16,7 @@ const Login = () => {
   const auth = getAuth(app);
   const [login, setLogin] = useState(false);
   const [email, setEmail] = useState("");
+  const [user, setUser] = useState({});
   const handleSignIn = (event) => {
     event.preventDefault();
     setLogin(false);
@@ -22,7 +27,6 @@ const Login = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
-
         setLogin(true);
         toast("Login Success", { autoClose: 1000 });
         form.reset();
@@ -46,6 +50,41 @@ const Login = () => {
         toast.success("Password Reset Email Sent", { autoClose: 1000 });
       });
     }
+  };
+  const loginWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const loginWithTwitter = () => {
+    const provider = new TwitterAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        console.error(error);
+        setUser({});
+      });
   };
   return (
     <div>
@@ -97,6 +136,44 @@ const Login = () => {
               <button onClick={forgetPass}>Click Here</button>
             </span>
           </p>
+        </div>
+        {user.uid ? (
+          <div
+            className="text-center border w-9/12 mx-auto my-4 border-blue-500 py-2 cursor-pointer"
+            onClick={handleSignOut}
+          >
+            <div>
+              <p>Sign Out</p>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div
+              className="text-center border w-9/12 mx-auto my-4 border-blue-500 py-2 cursor-pointer"
+              onClick={loginWithGoogle}
+            >
+              <div>
+                <p>Sign In With Google</p>
+              </div>
+            </div>
+            <div
+              className="text-center border w-9/12 mx-auto my-4 border-blue-500 py-2 cursor-pointer"
+              onClick={loginWithTwitter}
+            >
+              <div>
+                <p>Sign In With Twitter</p>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="flex justify-center text-center">
+          {user.providerData.forEach((profile) => {
+            console.log("Sign-in provider: " + profile.providerId);
+            console.log("  Provider-specific UID: " + profile.uid);
+            console.log("  Name: " + profile.displayName);
+            console.log("  Email: " + profile.email);
+            console.log("  Photo URL: " + profile.photoURL);
+          })}
         </div>
       </div>
     </div>
